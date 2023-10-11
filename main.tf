43a2073227d1bb81ec1b9c1b45296925c0bb82db
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-11 07:54:36 +0530

    Expense Scripts

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
 resource "aws_ssm_parameter" "params" {
   count = length(var.parameters)
-  name = var.parameters[count.index].name
-  type = var.parameters[count.index].type
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
 }
 
 variable "parameters" {
   default = [
-    {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
-    {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
-    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.rdevopsb73.online/", type = "String"},
-    {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com", type = "String"},
+    { name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString" },
+    { name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String" },
+    { name = "prod.expense.frontend.backend_url", value = "http://backend-prod.rdevopsb73.online/", type = "String" },
+    { name = "prod.rds.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com", type = "String" },
 
     ## Dev env
-    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.rdevopsb73.online/", type = "String"}
+    { name = "dev.expense.frontend.backend_url", value = "http://backend-dev.rdevopsb73.online/", type = "String" },
+
+    ## Common env
+    { name = "jenkins_password", value = "admin123", type = "String" }
   ]
 }
 
