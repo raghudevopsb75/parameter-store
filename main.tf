6338b16745188fd02ae885acc66aa2c5f287a3d7
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-16 08:44:49 +0530

    [D75-5] | Raghu K | Changes for MySQL

--- main.tf
+++ main.tf
@@ -40,6 +40,11 @@ variable "parameters" {
     { name = "dev.roboshop.cart.REDIS_HOST", value = "redis-dev.rdevopsb73.online", type = "String" },
     { name = "dev.roboshop.cart.CATALOGUE_HOST", value = "catalogue-dev.rdevopsb73.online", type = "String" },
     { name = "dev.roboshop.cart.CATALOGUE_PORT", value = "8080", type = "String" },
+
+
+    ## Passwords
+    { name = "dev.roboshop.mysql.DB_PASS", value = "RoboShop@1", type = "SecureString" },
+
   ]
 }
 
